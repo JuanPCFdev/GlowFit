a4d22655f1d7d625b1c19dfb33ac02f1926d9e7a
DanielBohorquezBT <daniel.bohorquez@bodytechcorp.com>
2025-05-27 10:15:54 -0500

    fix: iOS run app configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:glow_fit_app/features/gym/presentation/layouts/Login/login.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  try {
+    // Initialize Firebase using the generated configuration
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (e) {
+    // Handle initialization errors here
+  }
   runApp(MainApp());
 }
 
